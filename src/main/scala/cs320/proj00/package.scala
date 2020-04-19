1f5c25ac89bba7422e5ba0a92b3c86a0c41e02a0
pacokwon <haechank@gmail.com>
2020-04-19 22:00:25 +0900

    add(proj00) - recursive functions implemented

--- src/main/scala/cs320/proj00/package.scala
+++ src/main/scala/cs320/proj00/package.scala
@@ -61,6 +61,16 @@ package object proj00 extends Project00 {
       case _ => error(s"wrong type: $c is not BooleanV")
     }
 
+  def recHelper(ds: List[FunDef], b: Expr, env: Env): Value = {
+    val envWithFuncs = ds.foldLeft(env)((envAcc, definition) => {
+      val function = CloV(definition.ps, definition.b, envAcc)
+      function.env = envAcc + (definition.n -> function)
+      envAcc + (definition.n -> function)
+    })
+
+    interp(b, envWithFuncs)
+  }
+
   def interp(exp: Expr, env: Env): Value =
     exp match {
       case IntE(n) => IntV(n)
@@ -84,105 +94,106 @@ package object proj00 extends Project00 {
       case App(f, as) => appHelper(interp(f, env), as.map(arg => interp(arg, env)))
       case Test(e, t) => typeHelper(interp(e, env), t)
       case If(c, t, f) => condHelper(interp(c, env), t, f, env)
+      case RecFuns(ds, b) => recHelper(ds, b, env)
     }
 
   def tests: Unit = {
-    // // test-int
-    // test(run("42"), "42")
-    // // test-add
-    // test(run("1 + 2"), "3")
-    // // test-sub
-    // test(run("7 - 2"), "5")
-    // // test-mul
-    // test(run("2 * 4"), "8")
-    // // test-div
-    // test(run("5 / 2"), "2")
-    // // test-mod
-    // test(run("13 % 5"), "3")
-    // // test-neg
-    // test(run("1 - -1"), "2")
+    // test-int
+    test(run("42"), "42")
+    // test-add
+    test(run("1 + 2"), "3")
+    // test-sub
+    test(run("7 - 2"), "5")
+    // test-mul
+    test(run("2 * 4"), "8")
+    // test-div
+    test(run("5 / 2"), "2")
+    // test-mod
+    test(run("13 % 5"), "3")
+    // test-neg
+    test(run("1 - -1"), "2")
 
-    // // test-boolean
-    // test(run("true"), "true")
-    // // test-eq
-    // test(run("1 == 3 - 2"), "true")
-    // // test-lt
-    // test(run("1 < 3 - 2"), "false")
+    // test-boolean
+    test(run("true"), "true")
+    // test-eq
+    test(run("1 == 3 - 2"), "true")
+    // test-lt
+    test(run("1 < 3 - 2"), "false")
 
-    // // test-tuple1
-    // test(run("(1, 2 + 3, true)"), "(1, 5, true)")
-    // // test-tuple2
-    // test(run("((42, 3 * 2), false)"), "((42, 6), false)")
-    // // test-proj1
-    // test(run("(1, 2 + 3, true)._1"), "1")
-    // // test-proj2
-    // test(run("((42, 3 * 2), false)._1._2"), "6")
+    // test-tuple1
+    test(run("(1, 2 + 3, true)"), "(1, 5, true)")
+    // test-tuple2
+    test(run("((42, 3 * 2), false)"), "((42, 6), false)")
+    // test-proj1
+    test(run("(1, 2 + 3, true)._1"), "1")
+    // test-proj2
+    test(run("((42, 3 * 2), false)._1._2"), "6")
 
-    // // test-nil
-    // test(run("Nil"), "Nil")
-    // // test-cons
-    // test(run("1 :: 1 + 1 :: Nil"), "(1 :: (2 :: Nil))")
-    // // test-isempty1
-    // test(run("Nil.isEmpty"), "true")
-    // // test-isempty2
-    // test(run("(1 :: Nil).isEmpty"), "false")
-    // // test-head
-    // test(run("(1 :: Nil).head"), "1")
-    // // test-tail
-    // test(run("(1 :: Nil).tail"), "Nil")
-    // // test-tail-head
-    // test(run("(1 :: 2 :: Nil).tail.head"), "2")
+    // test-nil
+    test(run("Nil"), "Nil")
+    // test-cons
+    test(run("1 :: 1 + 1 :: Nil"), "(1 :: (2 :: Nil))")
+    // test-isempty1
+    test(run("Nil.isEmpty"), "true")
+    // test-isempty2
+    test(run("(1 :: Nil).isEmpty"), "false")
+    // test-head
+    test(run("(1 :: Nil).head"), "1")
+    // test-tail
+    test(run("(1 :: Nil).tail"), "Nil")
+    // test-tail-head
+    test(run("(1 :: 2 :: Nil).tail.head"), "2")
 
-//     // test-local1
-//     test(run("""
-//       val x = 1 + 2;
-//       val y = x * 4 + 1;
-//       y / (x - 1)
-//     """), "6")
-//     // test-local2
-//     test(run("""
-//       val (x, y) = (1 + 2, 3 + 4);
-//       val z = x * y;
-//       val (a, b, c) = (z, z + z, z + z + z);
-//       c - b
-//     """), "21")
+    // test-local1
+    test(run("""
+      val x = 1 + 2;
+      val y = x * 4 + 1;
+      y / (x - 1)
+    """), "6")
+    // test-local2
+    test(run("""
+      val (x, y) = (1 + 2, 3 + 4);
+      val z = x * y;
+      val (a, b, c) = (z, z + z, z + z + z);
+      c - b
+    """), "21")
 
-//     // test-fun
-//     test(run("x => x + x"), "<function>")
-//     // test-app1
-//     test(run("(x => x + x)(1)"), "2")
-//     // test-app2
-//     test(run("(x => y => x + y)(1)(2)"), "3")
-//     // test-app3
-//     test(run("((x, y) => x + y)(1, 2)"), "3")
+    // test-fun
+    test(run("x => x + x"), "<function>")
+    // test-app1
+    test(run("(x => x + x)(1)"), "2")
+    // test-app2
+    test(run("(x => y => x + y)(1)(2)"), "3")
+    // test-app3
+    test(run("((x, y) => x + y)(1, 2)"), "3")
 
-//     // test-type1
-//     test(run("1.isInstanceOf[Int]"), "true")
-//     // test-type2
-//     test(run("1.isInstanceOf[Boolean]"), "false")
-//     // test-type3
-//     test(run("(1 :: Nil).isInstanceOf[List]"), "true")
-//     // test-type4
-//     test(run("(x => x + x).isInstanceOf[Function]"), "true")
+    // test-type1
+    test(run("1.isInstanceOf[Int]"), "true")
+    // test-type2
+    test(run("1.isInstanceOf[Boolean]"), "false")
+    // test-type3
+    test(run("(1 :: Nil).isInstanceOf[List]"), "true")
+    // test-type4
+    test(run("(x => x + x).isInstanceOf[Function]"), "true")
 
-//     // test-if
-//     test(run("if (true) 1 else 2"), "1")
-//     // test-not
-//     test(run("!true"), "false")
-//     // test-and
-//     test(run("true && false"), "false")
-//     // test-or
-//     test(run("true || false"), "true")
-//     // test-neq
-//     test(run("1 != 2"), "true")
-//     // test-lte
-//     test(run("1 <= 1"), "true")
-//     // test-gt
-//     test(run("1 > 1"), "false")
-//     // test-gte
-//     test(run("1 >= 1"), "true")
-//     // test-nonempty
-//     test(run("Nil.nonEmpty"), "false")
+    // test-if
+    test(run("if (true) 1 else 2"), "1")
+    // test-not
+    test(run("!true"), "false")
+    // test-and
+    test(run("true && false"), "false")
+    // test-or
+    test(run("true || false"), "true")
+    // test-neq
+    test(run("1 != 2"), "true")
+    // test-lte
+    test(run("1 <= 1"), "true")
+    // test-gt
+    test(run("1 > 1"), "false")
+    // test-gte
+    test(run("1 >= 1"), "true")
+    // test-nonempty
+    test(run("Nil.nonEmpty"), "false")
 
     // test-rec1
     test(run("""
